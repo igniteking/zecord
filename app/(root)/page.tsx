d55a0b4c6fbcce793d3dee0a026a2a6c6c958c8f
Micheal <khanzaidan786@gmail.com>
2025-05-16 14:47:01 +0530

    init

---
+++ app/(root)/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <h1 className='text-2xl font-karla'>Welcome to loom clone</h1>
+  )
+}
+
+export default Page
---
+++ components/Navbar.tsx
@@ -0,0 +1,46 @@
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const user = {};
+const Navbar = () => {
+  return (
+    <header className="navbar">
+      <nav>
+        <Link href="/">
+          <Image
+            src="/assets/icons/logo.svg"
+            alt="logo"
+            width={32}
+            height={32}
+          ></Image>
+          <h1>Zecord</h1>
+        </Link>
+        {user && (
+          <figure>
+            <button>
+              <Image
+                src="/assets/images/dummy.jpg"
+                alt="user"
+                width={36}
+                height={36}
+                className="rounded-full aspect-square"
+              ></Image>
+            </button>
+            <button className="cursor-pointer">
+              <Image
+                src="/assets/icons/logout.svg"
+                alt="logout"
+                width={24}
+                height={24}
+                className="rotate-180"
+              ></Image>
+            </button>
+          </figure>
+        )}
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
