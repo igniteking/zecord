c4083e683ed01d0cbb0c58148e8e05dc26c0c142
Micheal <khanzaidan786@gmail.com>
2025-07-04 16:09:22 +0530

    auth completed

---
+++ app/(root)/upload/page.tsx
@@ -0,0 +1,14 @@
+import FileInput from '@/components/FileInput'
+import FormFeild from '@/components/FormFeild'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='wrapper page'>
+        <FormFeild />
+        <FileInput />
+    </div>
+  )
+}
+
+export default page
